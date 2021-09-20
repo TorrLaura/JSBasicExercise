534b13d97430f6065956f46590956a9a99928e8c
“TorrLaura” <laplebe35@gmail.com>
2021-09-20 17:58:15 -0500

    Completed JaveScriptBasicExercise

---
+++ script.js
@@ -0,0 +1,26 @@
+1. Use a console log to have your favorite number display in the console/terminal
+2. Use a comment to type out the 5 main data types (AKA Primitives)
+3. Use a comment to type out the 6 operator symbols
+4. Use modulo to get a remainder of 1 from the number 5. (There are two answers for this one. Please provide both. Also, please type them both out. Do not just type out the two numbers)
+5. Using single quotes at the start and end of the string have the words I'M MARY POPPINS, Y'ALL! display in the terminal/console (Must use a console log for this one.)
+6. Using Concatenation combine the following strings into one string. First string: "Hawk". Second string: "eye".
+7. Create a string for your first name and also your last name. Use the length property to add them together (Answer should be a number)
+8. Use index on the following string to select the lowercase "e" at the end of the string. "Avengers: Endgame". (Use a console log to display it in the console/terminal)
+
+console.log(9);
+
+/* 
+Primitives
+numbers
+strings
+Booleans
+Null
+Undefined
+/*
+
+/*
+Operators
+Adding & Subtracting
+Multiplying & Dividing
+Exponentiation
+Modulus
