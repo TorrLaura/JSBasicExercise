998dd9abfe9eed92bcf052b4132b94c6c4112a0b
“TorrLaura” <laplebe35@gmail.com>
2021-09-21 09:49:03 -0500

    corrected JSBasicExercise scrip.js file, completed bonus section during class review

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
-1. Use a console log to have your favorite number display in the console/terminal
-2. Use a comment to type out the 5 main data types (AKA Primitives)
-3. Use a comment to type out the 6 operator symbols
-4. Use modulo to get a remainder of 1 from the number 5. (There are two answers for this one. Please provide both. Also, please type them both out. Do not just type out the two numbers)
-5. Using single quotes at the start and end of the string have the words I'M MARY POPPINS, Y'ALL! display in the terminal/console (Must use a console log for this one.)
-6. Using Concatenation combine the following strings into one string. First string: "Hawk". Second string: "eye".
-7. Create a string for your first name and also your last name. Use the length property to add them together (Answer should be a number)
-8. Use index on the following string to select the lowercase "e" at the end of the string. "Avengers: Endgame". (Use a console log to display it in the console/terminal)
 
 console.log(9);
 
-/* 
-Primitives
-numbers
-strings
-Booleans
-Null
-Undefined
-/*
+//Numbers, Strings, Booleans, Null, & Undefined
 
-/*
-Operators
-Adding & Subtracting
-Multiplying & Dividing
-Exponentiation
-Modulus
+//Addition is +, Subtraction is -, Multiplication is *,
+ //Division is /, Exponentiation is **, Modulus is %
+
+console.log(5%4);
+console.log(5%2);
+console.log(1%5);
+
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+console.log("Hawk" + "Eye");
+console.log("Laura".length + "Torres".length);
+console.log("Avengers: Endgame"[16]);
+
+//BONUS
+
+console.log("\\");
+console.log(5 ** 4 - 9);
+console.log("I".length - "AM".length + "Iron".length - "Man".length);
+
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR LORD"[0]);
